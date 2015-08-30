fea8e30a83de80af582a436165c04e20a8ef906d
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-08-30 01:59:13 -0400

    msm8660: Fix Screen Flickering
    Change-Id: Ia21e58d8e549bfc4c203e224a50040406196f132
    Signed-off-by: gimmeitorilltell <gimmeitorilltell@yahoo.com>

--- msm8660.mk
+++ msm8660.mk
@@ -215,5 +215,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/samsung/msm8660-common/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab 
 
+#HW Hack
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.hwc.mdpcomp.enable=false
+
 # Common Qualcomm hardware
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
