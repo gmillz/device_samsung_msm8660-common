c2872282fab79fb2021e73a577fe70176ba43c8d
Steve Kondik <steve@cyngn.com>
2015-11-01 15:58:00 -0500

    msm8660: Switch to Clang by default
    Change-Id: I97016bf134aea6d59dc8307f6543c1d2f3bbf53a
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,8 @@ TARGET_SPECIFIC_HEADER_PATH := device/samsung/msm8660-common/include
 # Platform
 TARGET_BOARD_PLATFORM := msm8660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-#TARGET_BUILD_VARIANT := userdebug
+TARGET_BUILD_VARIANT := userdebug
+USE_CLANG_PLATFORM_BUILD := true
 
 # Architecture
 TARGET_CPU_VARIANT := scorpion
